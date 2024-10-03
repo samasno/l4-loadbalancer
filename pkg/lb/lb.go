3885a5c158e7a80830100ebdbbffe4c0eb26695d
samasno <samasno@samasno.com>
2024-10-03 12:58:12 -0500

    no keepalive

--- pkg/lb/lb.go
+++ pkg/lb/lb.go
@@ -1,39 +1,148 @@
 package lb
 
-import "net"
+import (
+	"errors"
+	"net"
+
+	"github.com/samasno/l4-loadbalancer/pkg/srv"
+)
+
+type HealthMonitor interface {
+	AddHost(Host, ...string) error
+	IsAlive(Host) (bool, error)
+}
+
+type UpstreamManager interface {
+	Add(Host) error
+	Next() (Host, error)
+	Peek() Host
+	String() string
+	Count() int
+}
+
+type Host = string
 
 type Layer4LoadBalancer struct {
-	conns           map[string]net.Conn
-	HealthMonitor   string
-	UpstreamManager string
+	connections     map[string]net.Conn
+	HealthMonitor   HealthMonitor
+	UpstreamManager UpstreamManager
 }
 
-func (l *Layer4LoadBalancer) Balance(client *net.Conn) {
-	// get upstream
-	// open up connection to upstream
-	// strip protocol
-	// pipe between client and upstream
+func NewLoadBalancer(hm HealthMonitor, u UpstreamManager) *Layer4LoadBalancer {
+	l := &Layer4LoadBalancer{
+		connections:     map[string]net.Conn{},
+		HealthMonitor:   hm,
+		UpstreamManager: u,
+	}
+
+	return l
 }
 
-func (l *Layer4LoadBalancer) Pipe(up, down net.Conn) {
-	go pipe(up, down)
-	go pipe(down, up)
+func (l *Layer4LoadBalancer) Handle(conn net.Conn) {
+	l.Balance(conn)
 }
 
-func pipe(in, out net.Conn) {
-	defer in.Close()
-	defer out.Close()
+func (l *Layer4LoadBalancer) ListenAndServe(addr string) error {
+	tcp := srv.NewTcpServer(addr)
 
-	b := make([]byte, 1024)
-	for {
-		n, err := in.Read(b)
+	tcp.Handle(l)
+
+	return tcp.ListenAndServe()
+}
+
+func (l *Layer4LoadBalancer) Balance(client net.Conn) {
+	remote := client.LocalAddr().String()
+	l.connections[remote] = client
+
+	var host Host
+	c := l.UpstreamManager.Count()
+	var err error
+	var ok bool
+
+	for i := 0; i < c*2; i++ {
+		host, err = l.UpstreamManager.Next()
 		if err != nil {
-			break
+			continue
 		}
 
-		_, err = out.Write(b[:n])
+		ok, err = l.HealthMonitor.IsAlive(host)
 		if err != nil {
-			break
+			continue
+		}
+
+		if ok {
+			var upstream net.Conn
+			host, _, _ := normalizeURL(host)
+			upstream, err = connect(host)
+			if err != nil {
+				continue
+			}
+			local := upstream.LocalAddr().String()
+			l.connections[local] = upstream
+			l.pipe(upstream, client)
+			return
+		} else {
+			continue
 		}
 	}
+
+	if err == nil {
+		err = errors.New("HTTP/1.2 500 error\r\nConnection: close\r\nContent-Length: 0")
+	}
+
+	client.Write([]byte(err.Error()))
+	client.Close()
+}
+
+func (l *Layer4LoadBalancer) Close() {
+	for _, conn := range l.connections {
+		if conn != nil {
+			conn.Close()
+		}
+	}
+}
+
+func connect(host string) (net.Conn, error) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func (l *Layer4LoadBalancer) pipe(up, down net.Conn) error {
+	if up == nil && down == nil {
+		return errors.New("got nil client")
+	}
+
+	forwardStream := func(in, out net.Conn) {
+		defer func() {
+			o := out.LocalAddr().String()
+			delete(l.connections, o)
+
+			i := in.LocalAddr().String()
+			delete(l.connections, i)
+
+			in.Close()
+			out.Close()
+		}()
+
+		b := make([]byte, 1024)
+		for {
+			n, err := in.Read(b)
+			if err != nil {
+				break
+			}
+
+			_, err = out.Write(b[:n])
+			if err != nil {
+				break
+			}
+		}
+	}
+
+	go forwardStream(up, down)
+	go forwardStream(down, up)
+	return nil
 }
--- pkg/lb/lb_test.go
+++ pkg/lb/lb_test.go
@@ -5,11 +5,91 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"strings"
 	"testing"
 	"time"
 )
+
+func TestBalancing(t *testing.T) {
+	testAddrs := []Host{
+		"http://127.0.0.1:5000",
+		"http://127.0.0.1:5001",
+		"http://127.0.0.1:5002",
+		"http://127.0.0.1:5003",
+	}
+
+	closer := startTestServers(testAddrs)
+	defer closer()
+
+	hm := NewHealthchecker()
+
+	for _, h := range testAddrs {
+		err := hm.AddHost(h)
+		if err != nil {
+			log.Fatalf("health monitor: %s", err.Error())
+		}
+	}
+
+	um := NewRoundrobin()
+	for _, h := range testAddrs {
+		err := um.Add(h)
+		if err != nil {
+			log.Fatalf("round robin: %s", err.Error())
+		}
+	}
+
+	lb := NewLoadBalancer(hm, um)
+	defer lb.Close()
+
+	go func(lb *Layer4LoadBalancer) {
+		err := lb.ListenAndServe("127.0.0.1:8080")
+		if err != nil && err != net.ErrClosed {
+			t.Log(err.Error())
+			t.Fail()
+		}
+	}(lb)
+
+	time.Sleep(time.Duration(20) * time.Second)
+
+	hits := map[Host]int{}
+
+	for _, h := range testAddrs {
+		_, ep, _ := normalizeURL(h)
+		hits[ep] = 0
+	}
+
+	for i := 0; i < 20; i++ {
+		ok, msg, err := pingOk("127.0.0.1:8080")
+		if err != nil {
+			t.Log(err.Error())
+			t.Fail()
+			break
+		}
+
+		if !ok {
+			t.Logf("invalid response ping #%d", i)
+			t.Fail()
+			break
+		}
+
+		for k, _ := range hits {
+			h, _, _ := normalizeURL(k)
+			if strings.Contains(msg, h) {
+				hits[k]++
+			}
+		}
+	}
+
+	for k, v := range hits {
+		t.Logf("got %d hits on %s", v, k)
+		if v == 0 {
+			t.Fail()
+		}
+	}
+
+}
 
 func TestTestServer(t *testing.T) {
 	var testAddrs = []string{
@@ -50,6 +130,7 @@ func startTestServers(addrs []string) func() {
 	testServers := []*http.Server{}
 
 	for _, addr := range addrs {
+		addr, _, _ = normalizeURL(addr)
 		srv := newTestServer(addr)
 		testServers = append(testServers, srv)
 		go func(srv *http.Server) {
@@ -87,7 +168,15 @@ func newTestServer(addr string) *http.Server {
 
 func pingOk(addr string) (bool, string, error) {
 	addr = "http://" + addr
-	res, err := http.Get(addr)
+	req, _ := http.NewRequest("GET", addr, nil)
+
+	client := http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+
+	res, err := client.Do(req)
 	if err != nil {
 		return false, "", err
 	}
